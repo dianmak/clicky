0d9a921c4e571ab01887e8ddb0255a153d4b9eb2
Blue Tortoise <dianmak@icloud.com>
2019-08-03 14:12:48 -0400

    Updates

---
+++ precache-manifest.bc05e3ba32ad6d5a23f6dac1862b5c8f.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e84b9f85a0ed98b25e35a7027406f155",
+    "url": "/clicky/index.html"
+  },
+  {
+    "revision": "cabafc853178cb82da15",
+    "url": "/clicky/static/css/main.012e31d5.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/clicky/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "cabafc853178cb82da15",
+    "url": "/clicky/static/js/main.6c4e0e6c.chunk.js"
+  },
+  {
+    "revision": "e4363465db862e89c37e",
+    "url": "/clicky/static/js/runtime~main.c6956f21.js"
+  },
+  {
+    "revision": "55e553ef28356d3e329a41fc6de75d40",
+    "url": "/clicky/static/media/bubblegum.55e553ef.png"
+  },
+  {
+    "revision": "cd5456e47145c2acbd3c3710e63ad129",
+    "url": "/clicky/static/media/gingerbread.cd5456e4.png"
+  },
+  {
+    "revision": "a2dcf1fcb8b3dd859ed355ba38a20dad",
+    "url": "/clicky/static/media/iceking.a2dcf1fc.png"
+  },
+  {
+    "revision": "0c3b4862b9e7f1cc4bc3e5c4dd5eeb66",
+    "url": "/clicky/static/media/jake.0c3b4862.png"
+  },
+  {
+    "revision": "2b9cdbdbf0e1295ea0c346c2d0cdb41b",
+    "url": "/clicky/static/media/lemongrab.2b9cdbdb.png"
+  },
+  {
+    "revision": "acf18dc2df8809583d3b72db671ffee4",
+    "url": "/clicky/static/media/lich.acf18dc2.png"
+  },
+  {
+    "revision": "09d4c347682ae7496fecfd45c21c8f85",
+    "url": "/clicky/static/media/lsp.09d4c347.png"
+  },
+  {
+    "revision": "59e9acbd35a646e8c07f05457890ab33",
+    "url": "/clicky/static/media/marceline.59e9acbd.png"
+  },
+  {
+    "revision": "ef584cd0787dc5c3c56c813a45231186",
+    "url": "/clicky/static/media/rainicorn.ef584cd0.png"
+  },
+  {
+    "revision": "1588e3bf14e3bf619160f01d1fa3b03f",
+    "url": "/clicky/static/media/treetrunks.1588e3bf.png"
+  }
+]);
